549f3e52e360044a4637db13fc44a668fa67087d
IamXacer <phanton7487@gmail.com>
2025-08-14 19:57:35 +0200

    refetchOnMount / refetchOnFocus  refetchOnReconnect defaultOptions — гибкость

---
+++ src/app/entrypoint/main.tsx
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+import '../styles/reset.css'
+import App from '../../App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+    defaultOptions:{
+        queries:{
+            staleTime: Infinity,
+            gcTime:5 * 1000
+        }
+    }
+})
+
+createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+)
